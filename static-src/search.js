2cb79c588fac44e43990310cf2e5762b856f53eb
Mikhail Cruz Andrade <jailandrade@gmail.com>
2017-08-24 20:34:51 -0500

    enable support for search in news

--- static-src/search.js
+++ static-src/search.js
@@ -2,5 +2,7 @@ var hugolunr = require('lunr-hugo');
 var h = new hugolunr();
 h.setInput('../content/resources/**');
 h.setOutput('../static/resources/resources.json');
+h.setInput('../content/news/**');
+h.setOutput('../static/news/news.json');
 console.info('resources search done');
 h.index();
