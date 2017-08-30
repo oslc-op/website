407be09052ecfbe0284b772869b5249e314259d6
Mikhail Cruz Andrade <jailandrade@gmail.com>
2017-08-30 16:35:50 -0500

    functions to search across the site

--- static-src/site.js
+++ static-src/site.js
@@ -1,11 +1,11 @@
 const hugolunr = require('lunr-hugo');
-const _ = require('lodash');
-const fs = require('fs');
+
 const about = new hugolunr();
 const contribute = new hugolunr();
 const news = new hugolunr();
 const resources = new hugolunr();
 const home = new hugolunr();
+var site = new hugolunr();
 
 var result = {};
 var aboutJson = {};
@@ -34,37 +34,10 @@ home.setInput('../content/_index.md');
 home.setOutput('../static/home/home.json');
 home.index();
 
-fs.readFile('../static/about/about.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  aboutJson = data;
-})
+site.setInput('../content/**/**');
+site.setOutput('../static/site.json');
+site.index();
 
-fs.readFile('../static/resources/resources.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  resourcesJson = data;
-})
-
-fs.readFile('../static/contribute/contribute.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  contributeJson = data;
-})
-
-fs.readFile('../static/news/news.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  newsJson = data;
-})
-
-fs.readFile('../static/home/home.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  homeJson = data;
-})
-
-_.merge(result, homeJson, aboutJson, contributeJson, resourcesJson, newsJson);
-var r = JSON.stringify(result);
-
-fs.writeFile('../static/site.json', r, 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.info(data);
-})
+console.info(site);
 
 console.info('finish task')
