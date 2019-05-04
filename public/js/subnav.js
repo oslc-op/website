ee76864f73d1dd538b460f42b6979d1e2b60faef
Gabriel Fernandes de Araujo Costa <gabrielfdac@gmail.com>
2019-05-04 13:16:57 -0300

    Updating Production Branch

--- public/js/subnav.js
+++ public/js/subnav.js
@@ -53,49 +53,6 @@ $(document).ready(function(){
   //this event will compare the current window position with the next or previous element in comparisson to the current active one
   //then toggle the right element in the nav accordingly
   $(window).scroll(function () {
-<<<<<<< HEAD
-    if (scroll == 100) {
-      if ($(window).scrollTop() > nav.items[0].position) {
-        for(i = 0; i < nav.items.length; i++) {
-          if (i == nav.items.length - 1) {
-            if ($(".sub_nav--item.active").offset().top - 100 != $(".sub_nav--item:last-child").offset().top - 100) {
-              $(".sub_nav--item.active").removeClass('active');
-              $(".sub_nav--item:last-child").addClass('active');
-              window.location.hash = $(".sub_nav--item:last-child").attr('href');
-            }
-            scroll = i;
-          }else if ($(window).scrollTop() > nav.items[i].position && $(window).scrollTop() < nav.items[i+1].position) {
-            scroll = i;
-            if (nav.items[i].position != $(".sub_nav--item.active").offset().top - 100) {
-              $(".sub_nav--item.active").removeClass('active');
-              $(".sub_nav--item:nth-child("+(scroll+1)+")").addClass('active');
-              window.location.hash = $(".sub_nav--item:nth-child("+(scroll+1)+")").attr('href');
-            }
-            break;
-          }
-        }
-      }
-    } else if ((scroll == nav.items.length-1) && ($(window).scrollTop() < nav.items[scroll].position)){
-      $(".sub_nav--item:nth-child("+(scroll+1)+")").removeClass('active');
-      $(".sub_nav--item:nth-child("+(scroll)+")").addClass('active');
-      window.location.hash = $(".sub_nav--item:nth-child("+(scroll)+")").attr('href');
-      scroll--;
-    } else if ((scroll == 0) && ($(window).scrollTop() > nav.items[scroll+1].position)) {
-      $(".sub_nav--item:nth-child("+(scroll+1)+")").removeClass('active');
-      $(".sub_nav--item:nth-child("+(scroll+2)+")").addClass('active');
-      window.location.hash = $(".sub_nav--item:nth-child("+(scroll+2)+")").attr('href');
-      scroll++;
-    } else {
-      if (scroll !== 3 && $(window).scrollTop() > nav.items[scroll+1].position) {
-        $(".sub_nav--item:nth-child("+(scroll+1)+")").removeClass('active');
-        $(".sub_nav--item:nth-child("+(scroll+2)+")").addClass('active');
-        window.location.hash = $(".sub_nav--item:nth-child("+(scroll+2)+")").attr('href');
-        scroll++;
-      } else if (scroll !== -1 && $(window).scrollTop() < nav.items[scroll].position){
-        $(".sub_nav--item:nth-child("+(scroll+1)+")").removeClass('active');
-        $(".sub_nav--item:nth-child("+(scroll)+")").addClass('active');
-        window.location.hash = 'about';
-=======
     //if the position of the window surpased the next element
     if ($(window).scrollTop() > items[scroll+1].position) {
       console.log(scroll);
@@ -106,7 +63,6 @@ $(document).ready(function(){
     //if the position of the window is lower than the element
     } else if ($(window).scrollTop() < items[scroll-1].position) {
       if(scroll != 0)
->>>>>>> 66b0014f1b5896a2d8bcdf73dff95d6a69a1bff8
         scroll--;
       toggleNext();
     }
