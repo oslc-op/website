be213b28a2beecb797dc9666b7b924761c57d517
Andrew Berezovskyi <andriib@kth.se>
2019-06-28 16:15:43 +0200

    update the deploy script
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "Updating Production branch"
 git pull --rebase=false origin production
 
 echo "Merging changes from Master"
-git pull --rebase=false origin master
+git pull --rebase=false --no-edit origin master
 
 echo "Generate Hugo site"
 hugo -b http://open-services.net/ -t oslc
@@ -24,3 +24,5 @@ git push origin production
 echo "Updating server code"
 SCRIPT="cd ../var/www/oslc.co; git pull origin production; exit"
 ssh -A root@165.227.5.255 "${SCRIPT}"
+
+git checkout master
