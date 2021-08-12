e9af8578b68e86823504dd62d39a19641788d6a4
Andrew Berezovskyi <andriib@kth.se>
2021-08-12 16:21:15 +0200

    Deploy OS versions of RM and CM vocabs/shapes (#296)
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- bin/conneg.sh
+++ bin/conneg.sh
@@ -3,6 +3,6 @@ set -eExuo pipefail
 
 NSNAME="${1%.*}"
 
-riot -q --check --stop-warnings --formatted=rdfxml "${NSNAME}.ttl" > "${NSNAME}.rdf"
-riot -q --check --stop-warnings --formatted=ntriples "${NSNAME}.ttl" > "${NSNAME}.nt"
-riot -q --check --stop-warnings --formatted=jsonld "${NSNAME}.ttl" > "${NSNAME}.jsonld"
+riot -q --check --strict --formatted=rdfxml "${NSNAME}.ttl" > "${NSNAME}.rdf"
+riot -q --check --strict --formatted=ntriples "${NSNAME}.ttl" > "${NSNAME}.nt"
+riot -q --check --strict --formatted=jsonld "${NSNAME}.ttl" > "${NSNAME}.jsonld"
